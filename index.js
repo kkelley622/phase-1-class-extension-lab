165154b7346d8e48aa0d68c0e2ddb6289b3ad6e5
Kevin Kelley <kkelley622@gmail.com>
2022-04-23 09:46:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+class Polygon {
+
+    constructor() {
+        this.arrayOfSides = []
+    }
+
+    get countSides() {
+        return this.arrayOfSides.length; 
+    }
+
+    get perimeter() {
+        return this.arrayOfSides.reduce((partialSum, a) => partialSum + a, 0);
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        if(this.arrayOfSides[0] === this.arrayOfSides[1] === this.arrayOfSides[2] === this.arrayOfSides[3]) {
+            return true;
+        }
+    }
+    get area() {
+        if(isValid === true) {
+            return this.arrayOfSides[0] * this.arrayOfSides[1];
+        }
+    }
+}
